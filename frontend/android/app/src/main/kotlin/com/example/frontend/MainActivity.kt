83c8ba0197e1caf0d79b31b203fc5505421ebe6f
Julien Bourgeois <julien_bourgeois@yahoo.fr>
2026-07-28 19:18:54 +0200

    Fix android "context not initialized" error.
    android/MainActivity needs to be customized to load the rust library.

--- frontend/android/app/src/main/kotlin/com/example/frontend/MainActivity.kt
+++ frontend/android/app/src/main/kotlin/com/example/frontend/MainActivity.kt
@@ -2,4 +2,10 @@ package com.example.frontend
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    companion object {
+        init {
+            System.loadLibrary("rust_lib_frontend")
+        }
+    }
+}
