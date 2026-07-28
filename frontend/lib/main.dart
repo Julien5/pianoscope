d3ccd2c85602ddd6dde1beeae78e4c8d255f5b8e
Julien Bourgeois <julien_bourgeois@yahoo.fr>
2026-07-28 07:04:01 +0200

    first commit

---
+++ frontend/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:frontend/src/providers/midi_provider.dart';
+import 'package:frontend/src/rust/frb_generated.dart';
+import 'package:frontend/src/screens/device_list_screen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await RustLib.init();
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => MidiProvider()..init(),
+      child: const NanoApp(),
+    ),
+  );
+}
+
+class NanoApp extends StatelessWidget {
+  const NanoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Nano MIDI',
+      theme: ThemeData(colorSchemeSeed: Colors.indigo, useMaterial3: true),
+      home: const DeviceListScreen(),
+    );
+  }
+}
